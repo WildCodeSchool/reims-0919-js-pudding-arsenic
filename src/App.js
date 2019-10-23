8bcda73cef534ebafe38e8df3d2baddac3dd992c
Pierre Moulin <phv.moulin@gmail.com>
2019-10-23 17:37:28 +0200

    display ingredient from array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ShowIngredients from './components/ShowIngredients/ShowIngredients';
 
-const getApi ={
-  "product": {
+const product = {
     "image_front_url": "https://static.openfoodfacts.org/images/products/400/263/102/4222/front_fr.8.400.jpg",
     "generic_name_fr": "Bière Blonde",
     "nutriments": {"alcohol": 5},
-    "manufacturing_places": "Allemagne",
+    "manufacturing_places": "Allemagne"
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-
-
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  randomIngredientNumber() {
+    let numbers = [3, 4, 5, 6, 7]
+    let idNumbers = (Math.floor(Math.random() * 5))
+    return numbers[idNumbers]
+  }
+  render() {
+    return (
+      <div>
+        <header>
+        </header> 
+        <div>
+          <ShowIngredients {...product}></ShowIngredients>
+        </div>
+      </div>
+    )
+  }  
 }
 
 export default App;
