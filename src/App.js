c7fa126a6cf4f3c579d1c3fddf0cfc80b73b4706
Pascal <fedodev55@gmail.com>
2019-10-23 16:34:04 +0200

    create fictional data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const getApi ={
+  "product": {
+    "image_front_url": "https://static.openfoodfacts.org/images/products/400/263/102/4222/front_fr.8.400.jpg",
+    "generic_name_fr": "Bière Blonde",
+    "nutriments": {"alcohol": 5},
+    "manufacturing_places": "Allemagne",
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-    
+
 
       </header>
     </div>
