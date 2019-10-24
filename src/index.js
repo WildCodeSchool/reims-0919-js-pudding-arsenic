c19f663d5a2d4ec02c6e2ac0dc2d18b2571f62f8
Pierre Moulin <phv.moulin@gmail.com>
2019-10-24 14:40:19 +0200

    update elsint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable import/extensions */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
