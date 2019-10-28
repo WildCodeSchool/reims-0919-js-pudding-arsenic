376ecb9b6846978ad431543e05dd7e7c085de7ce
Pierre Moulin <phv.moulin@gmail.com>
2019-10-28 19:04:08 +0100

    fixed ShowIngredient component

--- src/components/ShowIngredients/ShowIngredients.js
+++ src/components/ShowIngredients/ShowIngredients.js
@@ -1,21 +1,24 @@
 import React from "react"
 import "./ShowIngredients.css"
 
-function ShowIngredients() {
-	return (
-		<div className="ingredientCard">
-			<img
-				className="productImage"
-				src={this.props.image_front_url}
-				alt={this.props.generic_name_fr}
-			/>
-			<div className="productInfos">
-				<p className="productName">{this.props.generic_name_fr}</p>
-				<p className="productOrigin">{this.props.manufacturing_places}</p>
-				<p className="nutriments">{this.props.nutriments.alcohol}° alcool</p>
+class ShowIngredients extends React.Component {
+	render() {
+		return (
+			<div className="ingredientCard">
+				<img
+					className="productImage"
+					src={this.props.image_front_url}
+					alt={this.props.generic_name_fr}
+				/>
+				<div className="productInfos">
+					<p className="productName">{this.props.generic_name_fr}</p>
+					<p className="productOrigin">{this.props.manufacturing_places}</p>
+					<p className="nutriments">{this.props.nutriments.alcohol}° alcool</p>
+				</div>
 			</div>
-		</div>
-	)
-}
+		)
+	}		
+}	
+
 
 export default ShowIngredients
