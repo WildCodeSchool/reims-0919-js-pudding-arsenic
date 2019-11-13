2df0219de55569f6bd5a8e65b2cd0cfdd0ed0343
Pierre Moulin <phv.moulin@gmail.com>
2019-11-13 16:02:07 +0100

    fixed cards for mobile

--- src/components/ShowIngredients/ShowIngredients.js
+++ src/components/ShowIngredients/ShowIngredients.js
@@ -26,34 +26,31 @@ toggleModal() {
 
 render(){
 	return (
-		<div className="ingredientCard">			
+		<div className="ingredientCard">				
 			<img
 				className="productImage"
 				src={this.props.image_front_url}
 				alt={this.props.generic_name_fr}
-			/>					
+			/>
 			<div className="productInfos">
-					
 				<p className="productName">{this.props.generic_name_fr}</p>
 				<p className="productOrigin">{this.props.manufacturing_places}</p>
-				<button 
-					type="button" 
-					className="openModal" 
-					style={{height:'30px', width:'100px'}} 
-					onClick={this.toggleModal}>
-					Plus d'infos ...
-          		</button>
+				<button type="button" className="openModal" style={{height:'30px', width:'100px'}} onClick={this.toggleModal}>
+				Plus d'infos ...
+				</button>
 			</div>
 			<button 
-				type="button"
-				className="close"				
+				type="button" 
+				className="close" 
+				style={{height:'30px', width:'30px'}}
 				onClick={this.onDeleteClick}>
 				<span>&times;</span>
 			</button>
-			<Modal
-				revele={this.state.revele}
-				cache={this.toggleModal}
-			/>	
+
+				<Modal
+					revele={this.state.revele}
+					cache={this.toggleModal}
+				/>	
 
 		</div>
 	)			
