82efaed1e3d040b0fc5dca88ae8a67d99b337b9d
Pierre Moulin <phv.moulin@gmail.com>
2019-11-13 10:03:55 +0100

    fixed conflicts for PR with dev

--- src/components/ShowIngredients/ShowIngredients.js
+++ src/components/ShowIngredients/ShowIngredients.js
@@ -1,7 +1,6 @@
 import React from "react"
 import "./ShowIngredients.css"
 
-<<<<<<< HEAD
 
 class ShowIngredients extends React.Component{
 
@@ -42,23 +41,6 @@ render(){
 		</div>
 	)			
 }}
-=======
-function ShowIngredients ({image_front_url, generic_name_fr, manufacturing_places}) {
-	return (
-		<div className="ingredientCard">
-			<img
-				className="productImage"
-				src={image_front_url}
-				alt={generic_name_fr}
-			/>
-			<div className="productInfos">
-				<p className="productName">{generic_name_fr}</p>
-				<p className="productOrigin">{manufacturing_places}</p>
-			</div>
-		</div>
-	)			
-}	
->>>>>>> dev
 
 
 export default ShowIngredients
