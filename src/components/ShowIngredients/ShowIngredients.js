ca6f85f017ccada882dd0e1d89b3373d5d64f9eb
Pierre Moulin <phv.moulin@gmail.com>
2019-11-13 14:00:21 +0100

    update cards backgrounds, close button style

--- src/components/ShowIngredients/ShowIngredients.js
+++ src/components/ShowIngredients/ShowIngredients.js
@@ -26,31 +26,34 @@ toggleModal() {
 
 render(){
 	return (
-		<div className="ingredientCard">
-				<button 
-					type="button" 
-					className="close" 
-					style={{height:'30px', width:'30px'}}
-					onClick={this.onDeleteClick}>
-                    <span>&times;</span>
-                </button>
+		<div className="ingredientCard">			
 			<img
 				className="productImage"
 				src={this.props.image_front_url}
 				alt={this.props.generic_name_fr}
-			/>
-				<div className="productInfos">
-					<p className="productName">{this.props.generic_name_fr}</p>
-					<p className="productOrigin">{this.props.manufacturing_places}</p>
-					<button type="button" className="openModal" style={{height:'30px', width:'100px'}} onClick={this.toggleModal}>
-                    Plus d'infos ...
-          </button>
-				</div>
-
-				<Modal
-					revele={this.state.revele}
-					cache={this.toggleModal}
-				/>	
+			/>					
+			<div className="productInfos">
+					
+				<p className="productName">{this.props.generic_name_fr}</p>
+				<p className="productOrigin">{this.props.manufacturing_places}</p>
+				<button 
+					type="button" 
+					className="openModal" 
+					style={{height:'30px', width:'100px'}} 
+					onClick={this.toggleModal}>
+					Plus d'infos ...
+          		</button>
+			</div>
+			<button 
+				type="button"
+				className="close"				
+				onClick={this.onDeleteClick}>
+				<span>&times;</span>
+			</button>
+			<Modal
+				revele={this.state.revele}
+				cache={this.toggleModal}
+			/>	
 
 		</div>
 	)			
