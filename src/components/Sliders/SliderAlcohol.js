a17ade4790b1216b1bcc20b9c60347ad92860f91
Pascal <fedodev55@gmail.com>
2019-11-13 14:13:56 +0100

    create button for choise alcohol

--- src/components/Sliders/SliderAlcohol.js
+++ src/components/Sliders/SliderAlcohol.js
@@ -12,27 +12,45 @@ class App extends React.Component {
       Taux: {
         min: 0,
         max: 0,
+        choiseAlcohol: '',
+        choiseNo: '',
       },
     };
   }
+
+  showSliders=()=>{
+    // style={display: this.state.choiseAlcohol ? 'none' : 'block' }
+      this.setState({
+        choiseAlcohol : false,
+        choiseNo : true
+      })
+    }
+    showNoAlcohol=()=>{
+      // style={display: this.state.choiseAlcohol ? 'none' : 'block' }
+        this.setState({
+          choiseAlcohol : true,
+          choiseNo : false,
+        })
+      }
 
   render() {
     return (
       <div className = "formSlider">
         <div className = "btnSelectAlcohol">
-          <h3>Choisissez votre Cocktail ....</h3>
-          <buton className = "btnS">Petit joueur</buton>
-          <buton className = "btnA">Diabol hique</buton>
+          <h3 className = "titlechoiseAlcohol">Choisissez votre Cocktail ....</h3>
+          <button className = "btnS" onClick={this.showNoAlcohol}>Sans Alcool</button>
+          <button className = "btnA" onClick={this.showSliders}>Diabol hique</button>
         </div>
-        <div className = "showSlider">
+        <div>
+          <h1 className = "showTitleNo" style={ {display: this.state.choiseAlcohol ? 'none' : 'block'} }>Petit joueur !</h1>
+        </div>
+        <div className = "showSlider" style={ {display: this.state.choiseAlcohol ? 'block' : 'none'} }>
           <form className="form">
             <InputRange
               draggableTrack
               maxValue={100}
               minValue={0}
               onChange={value => this.setState({ Taux: value })}
-              // onChangeComplete={value => console.log(value)}
-              //let Label={value => (value)}
               value={this.state.Taux} />
           </form>
           <h3>Le taux d'alcool de votre brevage sera compris entre {this.state.Taux.min} et {this.state.Taux.max}°</h3>
