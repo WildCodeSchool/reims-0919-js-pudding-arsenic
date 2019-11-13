3665a74b0394a0f7d48df5434098462e3ca94b72
Pierre Moulin <phv.moulin@gmail.com>
2019-11-13 10:13:06 +0100

    resolved conflicts

--- src/components/ShowIngredients/ShowIngredients.js
+++ src/components/ShowIngredients/ShowIngredients.js
@@ -2,45 +2,39 @@ import React from "react"
 import "./ShowIngredients.css"
 
 
-class ShowIngredients extends React.Component{
+class ShowIngredients extends React.Component {
 
-onDeleteClick=()=>{
-	this.props.deleteClickHandler()
-}
+	onDeleteClick=()=>{
+		this.props.deleteClickHandler()
+	}
 
-// onDeleteClick(){
-// 	this.setState({
-
-// 	})
-// }
-
-render(){
-	return (
-		<div className="ingredientCard">
-			{/* <i className='fa fa-window-close' style={{cursor:'pointer',float:'right', fontSize:'24px',paddingRight:'0'}}onClick={this.onDeleteClick}></i>
-			<i className='fas fa-undo-alt' style={{cursor:'pointer',float:'right', fontSize:'24px',paddingRight:'0'}}></i> */}
-			<img
-				className="productImage"
-				src={this.props.image_front_url}
-				alt={this.props.generic_name_fr}
-			/>
-				<div className="productInfos">
-					<p className="productName">{this.props.generic_name_fr}</p>
-					<p className="productOrigin">{this.props.manufacturing_places}</p>
-					<button type="button" className="openModal" style={{height:'30px', width:'100px'}}>
-                    Plus d'infos ...
-          </button>
-				</div>
-				<button 
-					type="button" 
-					className="close" 
-					style={{height:'30px', width:'30px'}}
-					onClick={this.onDeleteClick}>
-                    <span>&times;</span>
-        </button>
-		</div>
-	)			
-}}
+	render(){
+		return (
+			<div className="ingredientCard">
+				{/* <i className='fa fa-window-close' style={{cursor:'pointer',float:'right', fontSize:'24px',paddingRight:'0'}}onClick={this.onDeleteClick}></i>
+				<i className='fas fa-undo-alt' style={{cursor:'pointer',float:'right', fontSize:'24px',paddingRight:'0'}}></i> */}
+				<img
+					className="productImage"
+					src={this.props.image_front_url}
+					alt={this.props.generic_name_fr}
+				/>
+					<div className="productInfos">
+						<p className="productName">{this.props.generic_name_fr}</p>
+						<p className="productOrigin">{this.props.manufacturing_places}</p>
+						<button type="button" className="openModal" style={{height:'30px', width:'100px'}}>
+						Plus d'infos ...
+			</button>
+					</div>
+					<button 
+						type="button" 
+						className="close" 
+						style={{height:'30px', width:'30px'}}
+						onClick={this.onDeleteClick}>
+						<span>&times;</span>
+			</button>
+			</div>
+		)			
+	}}
 
 
 export default ShowIngredients
